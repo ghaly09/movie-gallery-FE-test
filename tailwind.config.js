ee1ecc4384f0cd3f515de8fd875c45d793dda077
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 03:13:58 +0700

    config(tailwind): change tailwind config content from root folder to src folder wtih format .ts, .tsx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss/types').Config} */
 const config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "./src/pages/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
