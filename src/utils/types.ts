1992fa355b15894efe87d7af5832144dfd5d8303
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 03:35:53 +0700

    feat(types): add Props Types for components

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+// Types for all variables
+
+// Type for children component
+export interface ChildrenProps {
+  children: React.ReactNode;
+}
+
+// Types for headerTitle Props
+export type headerTitleProps = {
+  headertitle: string;
+  description: string;
+};
