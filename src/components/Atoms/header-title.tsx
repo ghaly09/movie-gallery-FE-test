ac8fb21cd2c9e8fc9c21cbd17a383ad0f05e173e
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 23:53:28 +0700

    refactory(app): change documentation and props

--- src/components/Atoms/header-title.tsx
+++ src/components/Atoms/header-title.tsx
@@ -8,7 +8,7 @@ export default function HeaderTitle({
   return (
     // Title and description for header with props headertitle and description
     <div>
-      <h1 className="text-4xl font-extrabold leading-none md:leading-tight tracking-tighter md:text-[2.5rem] mb-2 md:mb-0">
+      <h1 className="capitalize text-4xl font-extrabold leading-none md:leading-tight tracking-tighter md:text-[2.8rem] mb-2 md:mb-0">
         {headertitle}
       </h1>
       <h2 className="text-2xl font-extrabold leading-1 md:leading-tight tracking-tighter md:text-4xl mb-4">
