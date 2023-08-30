d0224510e4bb04b590135fb0497f102d4934c115
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 23:57:44 +0700

    refactory(comp): change props name

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -2,13 +2,13 @@ import * as React from "react";
 import HeaderTitle from "../../Atoms/header-title";
 import { headerTitleProps } from "@/src/utils/types";
 import HeadeContainer from "../../Atoms/containers/header-container";
-import Search from "../../Molecules/search";
+import SearchBar from "../../Molecules/searchBar";
 
 export default function Header({ headertitle, description }: headerTitleProps) {
   return (
     <HeadeContainer>
       <HeaderTitle headertitle={headertitle} description={description} />
-      <Search />
+      <SearchBar />
     </HeadeContainer>
   );
 }
