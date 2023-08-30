c12f7beee76581bbadbe64591ca496a83e9bb7d5
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 07:54:01 +0700

    feat(Types): add types input. button, and button variants components

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,8 +5,26 @@ export interface ChildrenProps {
   children: React.ReactNode;
 }
 
+// Type of InputProps extended from React.InputHTMLAttributes
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
 // Types for headerTitle Props
 export type headerTitleProps = {
   headertitle: string;
   description: string;
+};
+
+// Types for button component
+export type buttonVariantsTypes = {
+  variant: {
+    default: string;
+    outline: string;
+    secondary: string;
+  };
+  size: {
+    default: string;
+    sm: string;
+    lg: string;
+  };
 };
