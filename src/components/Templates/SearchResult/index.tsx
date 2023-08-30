9f329bfb14c42230cf77493718d3f483a7c1c0a3
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 00:08:12 +0700

    feat(comp): make searchReasults component and apply fetching data

---
+++ src/components/Templates/SearchResult/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { poppins } from "@/src/utils/fonts";
+import clsx from "clsx";
+import Header from "../../Organisms/Header";
+import { headerTitleProps } from "@/src/utils/types";
+
+export default function SearchResult({
+  headertitle,
+  description,
+}: headerTitleProps) {
+  return (
+    <section
+      className={clsx(
+        `flex flex-col min-h-screen items-center justify-between px-3`,
+        poppins.className
+      )}
+    >
+      <div className="md:container grid items-center pb-8 pt-6 md:py-10 mt-10">
+        <Header headertitle={headertitle} description={description} />
+      </div>
+    </section>
+  );
+}
