7aca704b57b0169f3eb8709862fc407b3727b9be
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 23:58:57 +0700

    styles(comp): make style responsive component

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -2,21 +2,18 @@ import * as React from "react";
 import { poppins } from "@/src/utils/fonts";
 import clsx from "clsx";
 import Header from "../../Organisms/Header";
-import { Input } from "../../Atoms/input";
+import { headerTitleProps } from "@/src/utils/types";
 
-export default function Home() {
+export default function Home({ headertitle, description }: headerTitleProps) {
   return (
     <section
       className={clsx(
-        `flex flex-col min-h-screen items-center justify-between px-3`,
+        `relative flex flex-col min-h-screen items-center justify-between px-3`,
         poppins.className
       )}
     >
-      <div className="md:container grid items-center pb-8 pt-6 md:py-10">
-        <Header
-          headertitle="Welcome."
-          description="Millions of movies, TV shows and people to discover."
-        />
+      <div className="md:container grid items-center pb-8 pt-8 md:py-10 mt-10">
+        <Header headertitle={headertitle} description={description} />
       </div>
     </section>
   );
