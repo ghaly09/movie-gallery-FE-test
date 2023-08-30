d40a210cdd27ff1b91d52437a53b36267d1e304c
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-30 07:43:35 +0700

    refactory(Header): delete input component for using it to other component

--- src/components/Organisms/Header/index.tsx
+++ src/components/Organisms/Header/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import * as React from "react";
 import HeaderTitle from "../../Atoms/header-title";
 import { headerTitleProps } from "@/src/utils/types";
 import HeadeContainer from "../../Atoms/containers/header-container";
+import Search from "../../Molecules/search";
 
 export default function Header({ headertitle, description }: headerTitleProps) {
   return (
     <HeadeContainer>
       <HeaderTitle headertitle={headertitle} description={description} />
+      <Search />
     </HeadeContainer>
   );
 }
