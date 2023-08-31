2934b39471414d13f6cb650327ebabe77d8c3b9a
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 23:31:54 +0700

    refactory(comp): add Filter By Year, change props, add loading image and notFOund 404 alert

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -4,12 +4,16 @@ import clsx from "clsx";
 import Header from "../../Organisms/Header";
 import { headerTitleProps } from "@/src/utils/types";
 import CardMovie from "../../Molecules/cardMovie";
+import FilterBy from "../../Molecules/filter";
+import { useSelector } from "react-redux";
+import LoaderImage from "../../Molecules/loaderImage";
+import NotFround from "../../Atoms/notFround";
 
 export default function Home({
   headertitle,
   description,
-  dataPopular,
 }: Partial<headerTitleProps>) {
+  const { data, loading } = useSelector((state: any) => state.home);
   return (
     <section
       className={clsx(
@@ -20,20 +24,28 @@ export default function Home({
       <div className="md:container grid items-center pb-8 pt-8 md:py-10 mt-10">
         <Header headertitle={headertitle} description={description} />
 
-        <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-center gap-5 mt-16 sm:mt-32">
-          {dataPopular?.map((film: any) => (
-            <CardMovie
-              urlImage={`https://image.tmdb.org/t/p/w500${film?.poster_path}`}
-              title={film?.title}
-              year={film?.release_date}
-              rating={film?.vote_average}
-              id={film?.id}
-              type={film?.media_type}
-              key={film?.id}
-              saved={undefined}
-            />
-          ))}
-        </div>
+        <FilterBy className="mt-16 sm:mt-32" />
+        {loading === true ? (
+          <LoaderImage className="" />
+        ) : data.length === 0 || data.results.length === 0 ? (
+          <NotFround />
+        ) : (
+          <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-center gap-5">
+            {data.results?.map((film: any) => (
+              <CardMovie
+                urlImage={`https://image.tmdb.org/t/p/w500${film?.poster_path}`}
+                title={film?.title}
+                year={film?.release_date}
+                rating={film?.vote_average}
+                id={film?.id}
+                type={film?.media_type}
+                key={film?.id}
+                saved={undefined}
+              />
+            ))}
+            F
+          </div>
+        )}
       </div>
     </section>
   );
