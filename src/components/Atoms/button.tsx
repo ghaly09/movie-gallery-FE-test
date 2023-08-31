5253ca460059182d6e372f84a3728107615e192a
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 10:14:29 +0700

    styles(comp): change styles on button comp

--- src/components/Atoms/button.tsx
+++ src/components/Atoms/button.tsx
@@ -31,7 +31,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           variant,
           size,
           className,
-          "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#94a3b8] focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-[#ffffff]"
+          "inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#94a3b8] focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-[#ffffff]"
         )}
         ref={ref}
         {...props}
