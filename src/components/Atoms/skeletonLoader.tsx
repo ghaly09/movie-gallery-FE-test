416afb261fc0d5fc0eec75b5435305334d0cd831
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 08:56:51 +0700

    feat(comp): Make skeleton loader component for loading

---
+++ src/components/Atoms/skeletonLoader.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from "./skeleton";
+
+export default function SkeletonLoader() {
+  return (
+    <div className="flex flex-col items-center space-x-4">
+      <Skeleton className="w-60 lg:w-64 md:h-[400px] lg:h-80 rounded-xl" />
+      <div className="space-y-2 mt-4">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  );
+}
