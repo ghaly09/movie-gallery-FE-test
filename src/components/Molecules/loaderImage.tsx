7c465fadac2eebbe9c194bd193f429b103cd85df
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 23:29:12 +0700

    refactory(comp): change tags on loader and add props for className

--- src/components/Molecules/loaderImage.tsx
+++ src/components/Molecules/loaderImage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import SkeletonLoader from "../Atoms/skeletonLoader";
+import clsx from "clsx";
+import { classNameType } from "@/src/utils/types";
 
-export default function LoaderImage() {
+export default function LoaderImage({ className }: classNameType) {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-center gap-5 sm:mt-32">
+    <div
+      className={clsx(
+        "grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-center gap-5",
+        className
+      )}
+    >
       {[0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
         <SkeletonLoader key={index} />
       ))}{" "}
