ff9b71643ac5dbe078de09d263e1cfcee9e7bc97
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 23:40:56 +0700

    refactory(pages): change color progressBar and update getServerSideProps fetch API to Redux AsynkThunk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,9 @@ import { store } from "../stores/store";
 import { Provider } from "react-redux";
 import Layout from "../layouts";
 import NextNProgress from "nextjs-progressbar";
+import { colorLoadingBar } from "../configs/config";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const colorLoadingBar =
-    "linear-gradient(rgb(134, 239, 172), rgb(59, 130, 246), rgb(147, 51, 234))";
   return (
     <Provider store={store}>
       <NextNProgress
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,25 @@
 import { NextPage } from "next";
-import axios from "axios";
-import baseAPI from "../services/API/base-API";
-import { headerTitleProps } from "../utils/types";
 import dynamic from "next/dynamic";
+import * as React from "react";
+import { fetchDataHome } from "../stores/reducers/homeSlice";
+import { useAppDispatch } from "../stores/store";
 
-const Index: NextPage = ({ dataPopular }: Partial<headerTitleProps>) => {
+const Index: NextPage = () => {
   const Home = dynamic(() => import("../components/Templates/Home"));
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchDataHome(`movie/popular?language=en-US&page=1`));
+  }, [dispatch]);
+
   return (
     <>
       <Home
         headertitle="Welcome."
         description="Millions of movies, TV shows and people to discover."
-        dataPopular={dataPopular}
       />
     </>
   );
 };
 
 export default Index;
-
-export async function getServerSideProps() {
-  try {
-    const responsePopular = await axios.request(
-      baseAPI(`movie/popular?language=en-US&page=1`)
-    );
-
-    const dataPopular = responsePopular.data.results;
-
-    return {
-      props: {
-        dataPopular,
-      },
-    };
-  } catch (error) {
-    console.log("errorFetchingHomeAPI", error);
-    return {
-      props: {
-        dataPopular: [],
-      },
-    };
-  }
-}
