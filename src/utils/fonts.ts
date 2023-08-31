843e0609abb0c5410a58cbc2072571ed334736c5
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 08:49:56 +0700

    refactory(Store): add fonts for detail page

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,18 @@
-import { Poppins } from "next/font/google";
+import {
+  JetBrains_Mono as FontMono,
+  Inter as FontSans,
+  Poppins,
+} from "next/font/google";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const fontMono = FontMono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 export const poppins = Poppins({
   weight: "400",
