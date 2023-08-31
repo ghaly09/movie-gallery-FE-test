d62a2d15c350627344e8c81aebd9f6fbc1d40bfa
ghaly09 <rachmatghaly0997@gmail.com>
2023-08-31 08:47:07 +0700

    refactory(comp): update component Home to use in pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,43 @@
 import { NextPage } from "next";
-import Home from "../components/Templates/Home";
+import axios from "axios";
+import baseAPI from "../services/API/base-API";
+import { headerTitleProps } from "../utils/types";
+import dynamic from "next/dynamic";
 
-const Index: NextPage = () => {
+const Index: NextPage = ({ dataPopular }: Partial<headerTitleProps>) => {
+  const Home = dynamic(() => import("../components/Templates/Home"));
   return (
     <>
       <Home
         headertitle="Welcome."
         description="Millions of movies, TV shows and people to discover."
+        dataPopular={dataPopular}
       />
     </>
   );
 };
 
 export default Index;
+
+export async function getServerSideProps() {
+  try {
+    const responsePopular = await axios.request(
+      baseAPI(`movie/popular?language=en-US&page=1`)
+    );
+
+    const dataPopular = responsePopular.data.results;
+
+    return {
+      props: {
+        dataPopular,
+      },
+    };
+  } catch (error) {
+    console.log("errorFetchingHomeAPI", error);
+    return {
+      props: {
+        dataPopular: [],
+      },
+    };
+  }
+}
