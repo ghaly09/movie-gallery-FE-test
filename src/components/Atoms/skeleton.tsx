6d4600986f86ba66417d8865b5eb118b26670143
ghaly09 <rachmatghaly0997@gmail.com>
2023-09-02 00:52:31 +0700

    docs(skeleton): add Documentation for skeleton component

--- src/components/Atoms/skeleton.tsx
+++ src/components/Atoms/skeleton.tsx
@@ -2,6 +2,27 @@ import clsx from "clsx";
 import * as React from "react";
 
 // Make skeleton for loading components
+/**
+ * Skeleton Component
+ *
+ * The Skeleton component provides a loading placeholder with a pulsating animation.
+ * It's useful for indicating that content is being loaded, especially in asynchronous operations.
+ *
+ * @component
+ * @param {object} props - The Skeleton component props.
+ * @param {string} [props.className] - Additional CSS classes to apply to the skeleton.
+ * @param {...any} props.props - Any additional props you'd like to pass to the skeleton element.
+ * @returns {JSX.Element} - Returns the Skeleton component.
+ *
+ * @example
+ * // Basic usage:
+ * <Skeleton />
+ *
+ * @example
+ * // Using additional CSS classes:
+ * <Skeleton className="custom-class" />
+ */
+
 function Skeleton({
   className,
   ...props
