c3da7c9cf8d327328e21e21a3106bdbc35ffbabd
ghaly09 <rachmatghaly0997@gmail.com>
2023-09-01 07:43:23 +0700

    refactory(feat): change mapping component

--- src/components/Molecules/loaderImage.tsx
+++ src/components/Molecules/loaderImage.tsx
@@ -11,7 +11,7 @@ export default function LoaderImage({ className }: classNameType) {
         className
       )}
     >
-      {[0, 1, 2, 3, 4, 5, 6, 7].map((index) => (
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((index) => (
         <SkeletonLoader key={index} />
       ))}{" "}
     </div>
