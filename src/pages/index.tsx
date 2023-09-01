dace406e895a16a066df9c9aa0203625de5ad57f
ghaly09 <rachmatghaly0997@gmail.com>
2023-09-01 07:49:14 +0700

    refactory(feat): move data fetching to home comp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,9 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
 import * as React from "react";
-import { fetchDataHome } from "../stores/reducers/homeSlice";
-import { useAppDispatch } from "../stores/store";
 
 const Index: NextPage = () => {
   const Home = dynamic(() => import("../components/Templates/Home"));
-  const dispatch = useAppDispatch();
-
-  React.useEffect(() => {
-    dispatch(fetchDataHome(`movie/popular?language=en-US&page=1`));
-  }, [dispatch]);
 
   return (
     <>
